f093ff6b832f31419ce3f236c0bc1f0929da2444
cherezov_a <alexey.cherezov@psi.ch>
2026-04-13 11:37:52 +0200

    Updated EOS with data from (Parks, 1968)

--- example.py
+++ example.py
@@ -2,9 +2,15 @@
 import gcnr
 
 atm = 1.01325e+5 # Pa
-t = 18000 # K
+t = 20000 # K
 
+print ('(Ievlev, 1980)')
 for p in [100,105,110,120,130]:
-    d1 = gcnr.eos.U.query (p*atm, t) * 1.E-3 # g/cm3
-    print (f"T={t} K   P={p} bar => D[U]={d1:6.3f} g/cm3")
+    d1 = gcnr.eos.U.query (p*atm, t, method='ievlev')
+    print (f"T={t} K   P={p} bar => D[U]={d1:6.3f} kg/m3")
+
+print ('(Parks, 1968)')
+for p in [100,105,110,120,130]:
+    d1 = gcnr.eos.U.query (p*atm, t, method='parks')
+    print (f"T={t} K   P={p} bar => D[U]={d1:6.3f} kg/m3")
 
--- gcnr/eos.py
+++ gcnr/eos.py
@@ -4,31 +4,55 @@ import scipy.constants as const
 from scipy.interpolate import LinearNDInterpolator
 from scipy import optimize
 
-class EOS_U:
-    def __init__ (self):
+atm = 1.01325e+5 # Pa
 
-        filename = os.path.join (os.path.dirname (__file__), 'data', 'uranium_eos_ievlev.txt')
+class EOS:
+    def __init__(self, method):
+        self.filename = os.path.join (os.path.dirname (__file__), 'data', f'uranium_eos_{method:}.txt')
 
-        try:
-            p, t, d = np.loadtxt (filename).T
-        except FileNotFoundError:
-            raise FileNotFoundError(f"EOS data file not found at {filename}")
-
-        #p *= 1.01325e+5 # 1 atm     => 1 Pa
-        #d *= 1.00000e+3 # 1 g/cm3   => 1 kg/m3
-        #e *= 4.18400e+6 # 1 cal/g   => 1 J/kg
-        #cv*= 4.18400e+6 # 1 cal/g/K => 1 J/kg/K
-        #cp*= 4.18400e+6 # 1 cal/g/K => 1 J/kg/K
-
+    def setup (self, p, t, d):
         self.points = np.array ([p, t]).T
         self.values = d
         self.interp = LinearNDInterpolator (self.points, self.values)
 
-    def query (self, p, t):
+class U_Ievlev(EOS):
+    def __init__ (self):
 
-        atm = 1.01325e+5 # Pa
+        EOS.__init__(self, 'ievlev')
 
-        den = self.interp (p / atm, t) * 1.E+3 # kg/m3
+        p, t, d = np.loadtxt (self.filename).T
+
+        p *= atm   # Pa
+
+        self.setup(p, t, d)
+
+class U_Parks(EOS):
+    def __init__ (self):
+
+        EOS.__init__(self, 'parks')
+
+        p, t, d = np.loadtxt (self.filename).T
+
+        p *= 1.01325e+5 # 1 atm     => 1 Pa
+        d *= 1.00000e+3 # 1 g/cm3   => 1 kg/m3
+        #e *= 4.18400e+6 # 1 cal/g   => 1 J/kg
+        #cv*= 4.18400e+6 # 1 cal/g/K => 1 J/kg/K
+        #cp*= 4.18400e+6 # 1 cal/g/K => 1 J/kg/K
+
+        self.setup(p, t, d)
+
+
+class EOS_U:
+    def __init__(self):
+
+        self.methods = {
+            'parks': U_Parks(), 
+            'ievlev' : U_Ievlev(),
+        }
+
+    def query (self, p, t, method='ievlev'):
+
+        den = self.methods [method].interp (p, t) # kg/m3
 
         if np.isnan(den).any():
             print(f"Warning: Point ({p}, {t}) is outside the data boundary.")
